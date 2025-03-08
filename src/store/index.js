5b69b5596c444d4199d6da35d72781f76aba36fe
kariymam <59487728+kariymam@users.noreply.github.com>
2025-03-07 20:38:16 -0500

    move fetch call out of zustand store

--- src/store/index.js
+++ src/store/index.js
@@ -4,32 +4,16 @@ const userForecast = create((set, get) => ({
     forecastDataArray: new Set(),
     isLoading: false,
     error: null,
-    fetchData: async (url) => {
-        set({ isLoading: true, error: null }); // Set loading state
-        try {
-            const urls = [url.forecast, url.forecastHourly];
-            const responses = await Promise.all(urls.map((url) => fetch(url)));
-            const errors = responses.filter((response) => !response.ok);
-
-            if (errors.length > 0) {
-                throw errors.map((response) => new Error(`Failed to fetch: ${response.statusText}`));
-            }
-
-            const data = await Promise.all(responses.map((response) => response.json()));
-            const [forecastData, forecastHourlyData] = data;
-
-            const payload = {
-                forecast: url.forecast,
-                forecastHourly: url.forecastHourly,
-                forecastData,
-                forecastHourlyData,
-            };
-            set((state) => ({ forecastDataArray: [payload, ...state.forecastDataArray], isLoading: false })); // Update state and clear loading
-        } catch (errors) {
-            set({ error: errors, isLoading: false }); // Set error state and clear loading
-            console.error('Fetch errors:', errors);
+    setLoading: (errors) => {
+        if (errors) {
+            set({ error: errors, isLoading: false });
+        } else {
+            set({ isLoading: true, error: null }); // Set loading state
         }
     },
+    setData: (payload) => {
+        set((state) => ({ forecastDataArray: [payload, ...state.forecastDataArray], isLoading: false })); // Update state and clear loading
+    }
 }));
 
 /**
