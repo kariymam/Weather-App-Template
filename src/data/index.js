166e2ee8fade2fb0ca1277d425d173d90abc2a97
kariymam <59487728+kariymam@users.noreply.github.com>
2025-03-08 23:43:43 -0500

    Started weather list

--- src/data/index.js
+++ src/data/index.js
@@ -7,4 +7,56 @@ const msg = {
     server: "Server error",
   },
 };
+
+const weatherList = [
+  {
+    word: [
+      "Clear",
+      "Fair",
+    ],
+    image: ''
+  },
+  {
+    word: "Partly Sunny",
+    image: ''
+  },
+  {
+    word: "Partly Cloudy",
+    image: ''
+  },
+  {
+    word: "Mostly Cloudy",
+    image: ''
+  },
+  {
+    word: "Overcast",
+    image: ''
+  },
+  {
+    word: [
+      "Snow",
+      "Snow Showers",
+      "Thunderstorm Snow",
+      "Light Snow",
+      "Heavy Snow"
+    ],
+    image: ''
+  },
+  {
+    word: ["Ice Pellets", "Hail", "Snow Pellets"],
+    image: ''
+  },
+  {
+    word: "Windy",
+    image: ''
+  },
+  {
+    word: "Thunderstorm",
+    image: ''
+  },
+  {
+    word: "Rain",
+    image: ''
+  }
+]
 export { msg };
